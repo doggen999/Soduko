14c22f5274a78be08b28108c3642c4f57d4d177c
doggen999 <natan.williams@tretton37.com>
2019-04-22 09:27:04 +0200

    Gritty general layout

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Board from './components/board/board'
+import PlayerControls from './components/playercontrols/playercontrols'
 import styles from "./style.scss";
 
 import { board1, solution1 } from './mockdata'
 
-const App = () => <div className={styles.app}>
-  <Board displayValues={board1} solutionValues={solution1} />
-
-</div>
-</div >;
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <Board displayValues={board1} solutionValues={solution1} />
+      <PlayerControls className={styles.playerControls} />
+    </div>)
+}
 
 export default App;
