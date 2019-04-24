49cc5c236e361247504e8858df3b8a87206c30c2
doggen999 <natan.williams@tretton37.com>
2019-04-24 20:57:17 +0200

    Compensating lack of head with running feet

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,6 @@
 import { board1, solution1, board2, solution2, board3, solution3 } from "../utils/mockdata";
 
-export const initialState = { initialState: false, board: board1, solution: solution1 }
+export const initialState = { initialState: true, board: board1, solution: solution1 }
 
 export const actions = {
   easy: state => ({ ...initialState, initialState: true, board: board1, solution: solution1 }),
